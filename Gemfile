3a3bd1771ea2396d9ee3364beb3d8b74f56f722d
ArthurDelbosco <delbosco.arthur@gmail.com>
2018-11-14 17:28:16 +0100

    rails new

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 
+#front end
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
@@ -26,7 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'awesome_print'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    category:     'chinese',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '06.05.09.09.07',
+  },
+  {
+    name:         'Pizza East',
+    category:     'italian',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '06.05.09.09.07',
+  },
+  {
+    name:         'Takao Takano',
+    category:     'japanese',
+    address:      '33 Rue Malesherbes',
+    phone_number: '06.05.09.09.07',
+  },
+  {
+    name:         'Les Terrasses de Lyon',
+    category:     'french',
+    address:      '25 Montée Saint-Barthélémy',
+    phone_number: '06.05.09.09.07',
+  },
+  {
+    name:         'Burgundy Lounge',
+    category:     'belgian',
+    address:      '24 Quai Saint-Antoine',
+    phone_number: '06.05.09.09.07',
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
